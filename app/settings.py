75af541c1c1cdeab38fef906eeded212b8604bdf
grungreee <grungeee09@gmail.com>
2025-09-15 17:07:49 +0300

    Update environment configuration; change local and test flags to False, and update URL for production deployment.

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,9 @@
 from app.schemas import AccountData
 import uuid
 
-local: bool = True
-test: bool = True
+local: bool = False
+test: bool = False
 session: str = "" if not test else uuid.uuid1()
-url: str = "localhost:8000" if local else ""
+url: str = "localhost:8000" if local else "https://camel-gram.onrender.com"
 version: str = "1.0.0"
 account_data: AccountData | None = None
